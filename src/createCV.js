e5295c3c77f44bd3fd531d09d3356133255478b7
Max Clayton Clowes <max.clayton-clowes@pollen.co>
2020-10-29 19:25:11 +0000

    [feat] finished api for defining cv

--- src/createCV.js
+++ src/createCV.js
@@ -1,52 +1,52 @@
 import generatePDF from "./generate/pdf";
 
-const options = {
-  debug: true,
+const defaultOptions = {
+  debug: false,
   website: false,
+  primary: false,
   printOptions: {
     displayHeaderFooter: false,
   },
-};
-
-const primaryCV = 'engineering';
+}
 
 const variations = {
-  engineering: [
-    "./src/sections/header.md",
-    "./src/sections/introduction/engineering.md",
-    "./src/sections/experience.md",
-    "./src/sections/education.md",
-    "./src/sections/aboutme.md",
-  ],
-  product: [
-    "./src/sections/header.md",
-    "./src/sections/introduction/product.md",
-    "./src/sections/experience.md",
-    "./src/sections/education.md",
-    "./src/sections/aboutme.md",
-  ],
+  engineering: {
+    files: [
+      "./src/sections/header.md",
+      "./src/sections/introduction/engineering.md",
+      "./src/sections/experience.md",
+      "./src/sections/education.md",
+      "./src/sections/aboutme.md",
+    ],
+    options: {
+      ...defaultOptions,
+      website: true,
+      primary: true,
+      debug: true,
+    }
+  },
+  product: {
+    files: [
+      "./src/sections/header.md",
+      "./src/sections/introduction/product.md",
+      "./src/sections/experience.md",
+      "./src/sections/education.md",
+      "./src/sections/aboutme.md",
+    ],
+  }
 }
 
 const createCV = (variation) => {
-  console.log(`Creating ${variation} CV`)
-  if(variation === primaryCV) {
-    const destination = "./mcclowes_cv.pdf";
-
-    generatePDF(
-      variations[variation], 
-      destination, 
-      { ...options, website: true }
-    );
-
-    return null;
-  }
+  const { files, options = defaultOptions } = variations[variation]
   
-  const destination = "./mcclowes_cv_product.pdf";
+  const destination = options.primary
+    ? `./mcclowes_cv.pdf`
+    : `./mcclowes_cv_${variation}.pdf`;
 
   generatePDF(
-    variations[variation], 
+    files, 
     destination, 
-    options
+    options,
   );
 }
 
@@ -55,5 +55,5 @@ const cvs = process.argv.slice(2)
 if(cvs.length>0) {
   cvs.forEach(cv => createCV(cv))
 } else {
-  createCV(primaryCV);
+  createCV('engineering');
 }
