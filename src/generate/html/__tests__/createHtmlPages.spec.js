7ec09759c322b9f3f0b2dd4d5bd76ffd44e95118
Max Clayton Clowes <max.clayton-clowes@pollen.co>
2020-11-16 15:37:53 +0000

    [fix] markdown tests work

--- src/generate/html/__tests__/createHtmlPages.spec.js
+++ src/generate/html/__tests__/createHtmlPages.spec.js
@@ -1,23 +1,13 @@
 import createHtmlPages from "../createHtmlPages";
+import markdown from "./markdown.md";
 
-const MARKDOWN_SAMPLE = `
-  # Hello World
-
-  This is some _markdown_.
-
-  __FOR REAL!__
-`;
-
-const MARKDOWN_AS_HTML = `<div class=\"page\" id=\"p1\">
-  # Hello World
-
-  This is some _markdown_.
-
-  __FOR REAL!__
-</div>`;
+const MARKDOWN_AS_HTML = `<div class="page" id="p1"><h1 id="hello-world">Hello World</h1>
+<p>This is some <em>markdown</em>.</p>
+<p><strong>FOR REAL!</strong></p>
+</div>`
 
 describe("createHtmlPages", () => {
   it("generates correct html", () => {
-    expect(createHtmlPages(MARKDOWN_SAMPLE)).toBe(MARKDOWN_AS_HTML);
+    expect(createHtmlPages(markdown)).toBe(MARKDOWN_AS_HTML);
   });
 });
--- src/generate/html/__tests__/readMarkdownFile.spec.js
+++ src/generate/html/__tests__/readMarkdownFile.spec.js
@@ -1,13 +1,23 @@
 import readMarkdownFile from "../readMarkdownFile";
-import markdown from "./markdown.md";
+// import markdown from "./markdown.md";
 
 const MARKDOWN_OPTIONS_DEFAULT = {
   encoding: "utf8",
 };
 
-describe.skip("readMarkdownFile", () => {
+const MARKDOWN_AS_HTML = `<h1 id="hello-world">Hello World</h1>
+<p>This is some <em>markdown</em>.</p>
+<p><strong>FOR REAL!</strong></p>
+`
+
+describe("readMarkdownFile", () => {
   it("generates correct html", () => {
-    expect(readMarkdownFile(markdown, MARKDOWN_OPTIONS_DEFAULT)).toBe(
+    expect(
+      readMarkdownFile(
+        "src/generate/html/__tests__/markdown.md", 
+        MARKDOWN_OPTIONS_DEFAULT
+      )
+    ).toBe(
       MARKDOWN_AS_HTML
     );
   });
