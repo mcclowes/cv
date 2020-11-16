fcf2fe9fdc60299ea5247ed67211700e136efc5c
Max Clayton Clowes <max.clayton-clowes@pollen.co>
2020-11-16 15:44:37 +0000

    [feat] test main html generator and use snapshots

--- src/generate/html/__tests__/createHtmlPages.spec.js
+++ src/generate/html/__tests__/createHtmlPages.spec.js
@@ -1,13 +1,8 @@
 import createHtmlPages from "../createHtmlPages";
 import markdown from "./markdown.md";
 
-const MARKDOWN_AS_HTML = `<div class="page" id="p1"><h1 id="hello-world">Hello World</h1>
-<p>This is some <em>markdown</em>.</p>
-<p><strong>FOR REAL!</strong></p>
-</div>`
-
 describe("createHtmlPages", () => {
   it("generates correct html", () => {
-    expect(createHtmlPages(markdown)).toBe(MARKDOWN_AS_HTML);
+    expect(createHtmlPages(markdown)).toMatchSnapshot();
   });
 });
--- src/generate/html/__tests__/readMarkdownFile.spec.js
+++ src/generate/html/__tests__/readMarkdownFile.spec.js
@@ -1,14 +1,8 @@
 import readMarkdownFile from "../readMarkdownFile";
-// import markdown from "./markdown.md";
 
 const MARKDOWN_OPTIONS_DEFAULT = {
   encoding: "utf8",
 };
-
-const MARKDOWN_AS_HTML = `<h1 id="hello-world">Hello World</h1>
-<p>This is some <em>markdown</em>.</p>
-<p><strong>FOR REAL!</strong></p>
-`
 
 describe("readMarkdownFile", () => {
   it("generates correct html", () => {
@@ -17,8 +11,6 @@ describe("readMarkdownFile", () => {
         "src/generate/html/__tests__/markdown.md", 
         MARKDOWN_OPTIONS_DEFAULT
       )
-    ).toBe(
-      MARKDOWN_AS_HTML
-    );
+    ).toMatchSnapshot();
   });
 });
